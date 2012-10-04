fcb4850912a51056e900161a9f7140c68fe9b883
Web Developer <username@Lubuntu-VirtualBox.(none)>
2012-10-03 23:55:59 -0600

    Blog

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'aa01ee4272c36e9068d8a36758d8e4477b18ecdaf38559d3d0860394d7fbc270079d09acf5d7d7e390279fda8a5f569b73c3547d4be4f276a2df113940b65c8b'
